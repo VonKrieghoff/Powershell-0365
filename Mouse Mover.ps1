473812c50eb6786dfa7e159c0292b09b018185dc
MB <registermeto@outlook.com>
2022-04-08 17:28:44 +0300

    Create Mouse Mover.ps1

---
+++ Mouse Mover.ps1
@@ -0,0 +1,37 @@
+
+
+# BEfore u will be able to execute, u need to chenge esecyuution policy to unrestrericted:
+# Set-ExecutionPolicy unrestricted -Scope currentuser
+# Set back:
+# Set-ExecutionPolicy restricted -Scope currentuser
+cls
+Write-Output "
+          __     __
+         /  \~~~/  \
+   ,----(     ..    )
+  /      \__     __/
+ /|         (\  |(       Mouse Running
+^ \   /___\  /\_|         
+   |__|   |__|
+
+
+
+"
+
+
+
+Add-Type -AssemblyName System.Windows.Forms
+
+while ($true)
+{
+  $Positon = [System.Windows.Forms.Cursor]::Position
+  #$x = ($pos.X % 500) + 1
+  #$y = ($pos.Y % 500) + 1
+
+  $x = $Positon.X + 1
+  $y = $Positon.Y + 1
+  [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
+  Write-Output "Mouse is now at positon X: $X | Y: $Y"
+
+  Start-Sleep -Seconds 10
+}
