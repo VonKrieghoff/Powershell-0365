16216c16385b682475dda9bd043f449b91bb3e70
MB <moreaccess@outlook.com>
2022-10-18 11:29:53 +0300

    Update Change Default Browser to MS Edge.ps1

--- Change Default Browser to MS Edge.ps1
+++ Change Default Browser to MS Edge.ps1
@@ -45,38 +45,36 @@ while ($true)
 #Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice' -Name ProgId -Value 'MSEdgeHTM'
   
  
-# Set which one slould be default browser
+# Set which one slould be default browser:
 $SetAsDefaultBrowser = "MSEdgeHTM"
-#$SetAsDefaultBrowser = MSEdgeHTM
+#$SetAsDefaultBrowser = "ChromeHTML"
 
-# Get default browser that we have at this moment
+# Get default browser that we have at this moment:
 $HTTPBrowser = (Get-ItemProperty -Path Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice).ProgId
 $HTTPsBrowser = (Get-ItemProperty -Path Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice).ProgId
-Write-Progress -CurrentOperation "Default Browser is" (" $HTTPBrowser")
-# If default browser is not what we need then if in loop will change that.
+Write-Progress -Activity  "Default Browser is $HTTPBrowser"
+# If default browser is not what we need then if in loop will change that:
 if($HTTPBrowser -ne $SetAsDefaultBrowser)
 {
- #Set as Default Browser
+ #Set as Default Browser:
  #Write-Output $HTTPBrowser
- Write-Progress -CurrentOperation "Changing default browser to" (" $SetAsDefaultBrowser")
- #Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice' -Name ProgId -Value $SetAsDefaultBrowser
+ Write-Progress -Activity "Curent Default Browser is $HTTPBrowser, Changing default browser to $SetAsDefaultBrowser"
+ Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice' -Name ProgId -Value $SetAsDefaultBrowser
 }
-# else {
-#Write-Output "DEfault HTTP browser already is $HTTPBrowser" }
+# else {Write-Output "DEfault HTTP browser already is $HTTPBrowser" }
 
 
 if($HTTPSBrowser -ne $SetAsDefaultBrowser)
 {
  #Set as Default Browser
- Write-Output $HTTPBrowser
- #Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice' -Name ProgId -Value $SetAsDefaultBrowser
+ #Write-Output $HTTPBrowser
+ Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice' -Name ProgId -Value $SetAsDefaultBrowser
 }
-#else {
-#Write-Output "DEfault HTTPS browser already is $HTTPSBrowser" }
+#else {Write-Output "DEfault HTTPS browser already is $HTTPSBrowser" }
 
 
  
   
-  # Waiting 5s and doing everything all over.  
-  Start-Sleep -Seconds 5
+# Waiting 5s and doing everything all over.  
+Start-Sleep -Seconds 5
 }
